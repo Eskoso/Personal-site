2b7fe94f6c34c8c5e8c7ad670a53f1f4e016dec2
Walter Esko <esko@Eskos-Macbook-Air.local>
2024-12-16 23:07:04 +0100

    added a ball cursor

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,5 +13,40 @@ window.addEventListener("load", function () {
     }, 1600); 
 });
 
+
+const ball = document.createElement("div");
+ball.className = "cursor"
+document.body.appendChild(ball);
+
+const ballSize = 50; 
+
+document.addEventListener("mousemove", (e) => {
+    const x = e.clientX - ballSize / 2; 
+    const y = e.clientY - ballSize / 2;
+
+    ball.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+document.querySelectorAll("[data-hover-enabled]").forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+        ball.style.width = "55px"; 
+        ball.style.height = "55px"
+    });
+
+    element.addEventListener("mouseleave", () => {
+        ball.style.width = "45px"; 
+        ball.style.height = "45px" 
+    });
+});
+
+document.addEventListener("mousedown", () => {
+        ball.classList.add("clicked");
+});
+
+document.addEventListener("mouseup", () => {
+    ball.classList.remove("clicked");
+});
+
+
 var copy = document.querySelector(".middle-content").cloneNode(true);
 document.querySelector(".slider").appendChild(copy);
